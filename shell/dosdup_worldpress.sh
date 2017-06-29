b55542cce5f66ba69574c2f6d469d33a7a57ad1d
coffeehoock <coffeehoock@gmail.com>
2017-06-29 21:56:33 +0300

    "17-06-29 21:56:33"

--- shell/dosdup_worldpress.sh
+++ shell/dosdup_worldpress.sh
@@ -1,4 +1,7 @@
 sudo find . -type f -exec chmod 644 {} +
 sudo find . -type d -exec chmod 755 {} +
 
-
+if(is_admin()) {
+    add_filter('filesystem_method', create_function('$a', 'return "direct";' ));
+    define( 'FS_CHMOD_DIR', 0751 );
+}
