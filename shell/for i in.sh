497a6f63156014f5ea5e7dfcd649c49be7ff6920
coffeehoock <coffeehoock@gmail.com>
2017-09-18 23:09:25 +0300

    "17-09-18 23:09:25"

--- shell/for i in.sh
+++ shell/for i in.sh
@@ -2,3 +2,7 @@ for i in {0..10};   do touch "test_$i.html"; done
 
 j=0; for i in *.jpg; do let j+=1; mv $i $j.jpg ; done
 
+
+export i=1; for f in *; do mv "$f" ${i}.jpg; export i=`expr $i + 1`; done
+
+
