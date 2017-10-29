217434fdadb4db2dc2061769f1daed9071b09fa0
coffeehoock <coffeehoock@gmail.com>
2017-10-29 04:50:30 +0300

    "17-10-29 04:50:30"

--- shell/net.sh
+++ shell/net.sh
@@ -1,2 +1,7 @@
- sudo lsof -i
+sudo lsof -i | grep 80
+sudo lsof -i
 hostname -I -i
+sudo netstat -ntulp
+Список подключенных хостов
+
+netstat -lantp | grep ESTABLISHED |awk '{print $5}' | awk -F: '{print $1}' | sort -u
---
+++ shell/scp.sh
@@ -0,0 +1,2 @@
+scp  cooffeehoock@192.168.1.39:~/desktop/file1 .
+scp file2   cooffeehoock@192.168.1.39:~/desktop/
