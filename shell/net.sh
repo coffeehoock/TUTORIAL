fa2747fef1a1c34557693878df2081450d909528
coffeehoock <coffeehoock@gmail.com>
2017-10-29 20:57:57 +0300

    "17-10-29 20:57:57"

--- shell/net.sh
+++ shell/net.sh
@@ -1,7 +1,7 @@
 sudo lsof -i | grep 80
 sudo lsof -i
 hostname -I -i
-sudo netstat -ntulp
+netstat -lantp | grep ESTABLISHED |awk '{print $5}' | awk -F: '{print $1}' | sort -u
 Список подключенных хостов
 
 netstat -lantp | grep ESTABLISHED |awk '{print $5}' | awk -F: '{print $1}' | sort -u
--- shell/scp.sh
+++ shell/scp.sh
@@ -1,2 +1,3 @@
 scp  cooffeehoock@192.168.1.39:~/desktop/file1 .
 scp file2   cooffeehoock@192.168.1.39:~/desktop/
+ssh-copy-id -i ~/.ssh/id_rsa.pub cooffeehoock@192.168.1.39
