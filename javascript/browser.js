9d4e910d72bbf9eddcafba8fcc202b86f7738cd0
coffeehoock <coffeehoock@gmail.com>
2017-11-03 15:37:31 +0300

    Update browser.js

--- javascript/browser.js
+++ javascript/browser.js
@@ -1,95 +1,29 @@
-
-
-    function ifBrowser (obj) {
-
-        var none = obj["none"] || "none";
-
-        function cBrowser() {
-
-            var ua = navigator.userAgent;
-
-            var bName = function () {
-
-                if (ua.search(/MSIE/) > -1) return "ie";
-
-                if (ua.search(/Firefox/) > -1) return "firefox";
-
-                if (ua.search(/Opera/) > -1) return "opera";
-
-                if (ua.search(/Chrome/) > -1) return "chrome";
-
-                if (ua.search(/Safari/) > -1) return "safari";
-
-                if (ua.search(/Konqueror/) > -1) return "konqueror";
-
-                if (ua.search(/Iceweasel/) > -1) return "iceweasel";
-
-                if (ua.search(/SeaMonkey/) > -1) return "seamonkey";}();
-
-            var version = function (bName) {
-
-                switch (bName) {
-
-                    case "ie" : return (ua.split("MSIE ")[1]).split(";")[0];break;
-
-                    case "firefox" : return ua.split("Firefox/")[1];break;
-
-                    case "opera" : return ua.split("Version/")[1];break;
-
-                    case "chrome" : return (ua.split("Chrome/")[1]).split(" ")[0];break;
-
-                    case "safari" : return (ua.split("Version/")[1]).split(" ")[0];break;
-
-                    case "konqueror" : return (ua.split("KHTML/")[1]).split(" ")[0];break;
-
-                    case "iceweasel" : return (ua.split("Iceweasel/")[1]).split(" ")[0];break;
-
-                    case "seamonkey" : return ua.split("SeaMonkey/")[1];break;
-
-                }}(bName);
-
-            return [bName,bName + version.split(".")[0],bName + version];
-
-        }
-
-        var current_browser = cBrowser();
-
-        for (var key in obj) {
-
-            var trg = key.toLowerCase();
-
-            if (trg.indexOf(current_browser[2]) > -1) return obj[key];
-
-            else if (trg.indexOf(current_browser[1]) > -1) return obj[key];
-
-            else {
-
-                var nsymbol = trg.charAt(trg.indexOf(current_browser[0])+current_browser[0].length);
-
-                if (trg.indexOf(current_browser[0]) > -1 && (nsymbol == " " || nsymbol == "")) return obj[key];
-
-            };
-
-        }
-
-        return none;
-
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
+};
 
 
+if(isMobile.any()){
+//действие, которое нужно сделать, если браузер мобильный
+}
 
-var transform = ifBrowser({
-
-        "ie":"msTransform",
-
-        "chrome safari":"WebkitTransform",
-
-        "firefox":"MozTransform",
-
-        "opera":"OTransform",
-
-                         
-
-    }) // определяем название нужного нам css свойства
-
-    document.body.style[transform] = "rotate(25deg)"
+if(isMobile.iOS()){
+//действие, которое нужно сделать, если браузер iOS
+}
