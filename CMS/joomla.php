9da5a1f71978eeb19ee5596aa5516879ba566846
coffeehoock <coffeehoock@gmail.com>
2017-11-15 10:24:05 +0300

    Update joomla.php

--- CMS/joomla.php
+++ CMS/joomla.php
@@ -1,2 +1,3 @@
+<?php
     if( !$detect->isMobile() && !$detect->isTablet() && ((int)$detect->version('IE') == '' || (int)$detect->version('IE') > 8 ) && !$detect->version('iOS')){ ?>
     <script src="<?php echo JURI::base().'/templates/'.$this->template.'/js/jquery.simplr.smoothscroll.min.js'; ?>"></script>
