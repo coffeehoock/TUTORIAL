470cd1e00999e41fdb3399d08c34b45ac5bdcd34
coffeehoock <coffeehoock@gmail.com>
2017-12-26 23:40:02 +0300

    "17-12-26 23:40:02"

---
+++ sql/myscl.pro.sql
@@ -0,0 +1,18 @@
+SHOW processlist;
+
+CREATE TABLE index_test(
+  id INT AUTO_INCREMENT PRIMARY KEY, --PRIMARY KEY не повторять значение NOT NULL обязательное заполнение
+  name VARCHAR (30),
+  index_ID VARCHAR (10)
+);
+
+DESC index_test; --структура таблицы
+
+SELECT * FROM `index_test`;
+
+INSERT INTO index_test
+  (id,name,`index_ID`)
+  VALUES
+  (null,'pavel','russia');
+
+CREATE INDEX index_ID ON index_test (index_ID);
