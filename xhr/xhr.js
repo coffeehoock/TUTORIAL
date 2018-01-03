e3dc40a2115c3388b07d64d27bd428a2db111a6b
coffeehoock <coffeehoock@gmail.com>
2018-01-03 16:36:54 +0300

    add join

--- xhr/xhr.js
+++ xhr/xhr.js
@@ -1,32 +1,32 @@
 __FILE_INDEX__ = "resp.php";
-========
+/*========
 jQuery
-========
+========*/
 $.get( __FILE_INDEX__, { mynum: 77711144} , function( data ) {
   console.log(JSON.parse(data));
 });
 
-========
+/*========
 axios
-========
+========*/
 axios.post(__FILE_INDEX__, { mynum: 44})
   .then(function (response) {
     console.log(response.data.foo);
   })
 
-========
+/*========
 XMLHttpRequest
-========
+========*/
 var xhr = new XMLHttpRequest();
 xhr.open('GET', __FILE_INDEX__, false);
 xhr.send();
 xhr.response
   
-========
+/*========
 php resp and data
-========
+========*/
   
-<?php 
+/*<?php
 $mypost = $_REQUEST['mynum'];
 $my_array = array(
     "foo" => $mypost,
