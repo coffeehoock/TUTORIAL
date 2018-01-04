7aa1724e1cf6c7034e6884014a7a2cee5231314c
coffeehoock <coffeehoock@gmail.com>
2018-01-04 17:06:24 +0300

    join

--- sql/myscl.join.sql
+++ sql/myscl.join.sql
@@ -20,9 +20,12 @@ CREATE TABLE books(
 );
 
 INSERT INTO books VALUE (NULL ,'магия и кровь', 3);
+INSERT INTO books VALUE (NULL ,'магия и кровь 2', 3);
 INSERT INTO books VALUE (NULL ,'уникальная вселенная', NULL);
 INSERT INTO books VALUE (NULL ,'физика просто', 2);
 INSERT INTO books VALUE (NULL ,'рфссказ о цветах', 1);
+INSERT INTO books VALUE (NULL ,'рфссказ о цветах 2', 1);
+INSERT INTO books VALUE (NULL ,'рфссказ о цветах 3', 1);
 
 --Перекресный соединение CROSS
 SELECT b.book_name, p.publisher_name
@@ -30,9 +33,31 @@ FROM books b
 CROSS JOIN publishers p;
 
 --Естественное соединение NATURAL
-SELECT b.book_name, p.publisher_name
+SELECT b.book_name, p.publisher_name,
 FROM books b
 NATURAL JOIN publishers p;
+
+SELECT b.book_name, p.publisher_name, p.publisher_id
+FROM books b
+NATURAL JOIN publishers p;
+
+SELECT * FROM books NATURAL JOIN publishers;
+
+--внутренние соединение INNER
+SELECT b.book_name, p.publisher_name
+FROM books b
+INNER JOIN publishers p
+ON b.publisher_id = p.publisher_id; --ON b.publisher_id <> p.publisher_id;
+
+--внешнее соединение INNER
+SELECT books.book_name, publishers.publisher_name
+FROM books LEFT JOIN publishers
+ON books.book_id = publishers.publisher_id;
+
+SELECT books.book_name, publishers.publisher_name
+FROM books LEFT JOIN publishers
+USING (publisher_id);
+
 
 
 
