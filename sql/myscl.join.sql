902eeb2a310ed444d37ef306ba822cb8b186d572
coffeehoock <coffeehoock@gmail.com>
2018-01-04 16:25:46 +0300

    join

--- sql/myscl.join.sql
+++ sql/myscl.join.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE books_db;
+CREATE DATABASE books_db DEFAULT CHARSET=utf8 COLLATE  utf8_general_ci;
 USE books_db;
 
 CREATE TABLE publishers(
@@ -18,3 +18,16 @@ CREATE TABLE books(
   publisher_id INT,
   FOREIGN KEY (publisher_id) REFERENCES publishers (publisher_id)
 );
+
+INSERT INTO books VALUE (NULL ,'магия и кровь', 3);
+INSERT INTO books VALUE (NULL ,'уникальная вселенная', NULL);
+INSERT INTO books VALUE (NULL ,'физика просто', 2);
+INSERT INTO books VALUE (NULL ,'рфссказ о цветах', 1);
+
+--ререкресный метод
+SELECT b.book_name, p.publisher_name
+FROM books b
+CROSS JOIN publishers p;
+
+
+
