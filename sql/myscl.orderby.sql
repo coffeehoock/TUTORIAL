8668f9a1ed91e15b227f91ff4453e84979643fb0
coffeehoock <coffeehoock@gmail.com>
2018-01-07 00:20:14 +0300

    order by
    Signed-off-by: coffeehoock <coffeehoock@gmail.com>

--- sql/myscl.orderby.sql
+++ sql/myscl.orderby.sql
@@ -8,3 +8,32 @@ fnames_list VARCHAR(20),
 lnames_list VARCHAR(20),
 reputation INT
 );
+
+INSERT INTO users VALUES (NULL,'petr','van','500');
+INSERT INTO users VALUES (NULL,'lola','vin','324');
+INSERT INTO users VALUES (NULL,'jimi','voode','21');
+INSERT INTO users VALUES (NULL,'greg','edison','0');
+INSERT INTO users VALUES (NULL,'maik','gayden','2');
+
+SELECT * FROM users;
+
+DELETE FROM `users` WHERE ((`id` > '5'));
+
+
+UPDATE users
+SET rank = CASE
+WHEN reputation > 350 THEN 'elita'
+WHEN reputation > 100 THEN 'popular'
+WHEN reputation > 0   THEN 'user'
+WHEN reputation = 0   THEN 'junior'
+ELSE 'noob'
+END;
+
+SELECT fnames_list, reputation, rank
+FROM users
+ORDER BY fnames_list;
+
+SELECT fnames_list, reputation, rank
+FROM users
+ORDER BY reputation DESC;
+
