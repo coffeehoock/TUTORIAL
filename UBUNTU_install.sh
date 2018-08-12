559c439ed9af38e3d8e862065a947ee4e307673d
coffeehoock <coffeehoock@gmail.com>
2018-08-12 12:06:04 +0300

    Update UBUNTU_install.sh

--- UBUNTU_install.sh
+++ UBUNTU_install.sh
@@ -124,7 +124,7 @@ sudo atom /etc/passwd
 <userName>:x:0:0::/mnt/56f5f743-0eb2-4a7b-9b09-d7eb97dce628/user//env-html:
 
 sudo usermod -a -G root,sudo <userName>
-sudo passwd -d
+sudo passwd -d <userName>
 
 
 sudo chown -R  lowchat:commpier /home/lowchat
