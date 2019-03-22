fa0f2dec289ae4ac34a60e6dc3f9692554b441d6
coffeehoock <coffeehoock@gmail.com>
2019-03-22 17:01:57 +0300

    test

--- javascript/писк элементов.js
+++
@@ -1,9 +0,0 @@
-//динамические элементы
-var doc = document; //кеш
-
-var elemA, contA , roomA = [];
-elemA = doc.createElement("p");
-contA = doc.createTextNode("дядя вася");
-
-elemA.appendChild(contA);
-roomA.appendChild(elemA);
